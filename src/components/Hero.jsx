6e71721ddbd6aaa17a750b0c55bdab3873af60dc
mrigyanshii <mrigyanshidigari23@lpu.in>
2026-03-22 22:58:37 +0530

    pls last update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -72,7 +72,7 @@ const Hero = () => {
                             <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
                             <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
                         </svg>
-                        <span className="font-mono" style={{ fontSize: '0.875rem' }}>Also an published author for an anthology - Autumn Diaries (Amazon)</span>
+                        <span className="font-mono" style={{ fontSize: '0.875rem' }}>Published author- Autumn Diaries (Amazon anthology)</span>
                     </div>
 
                     <div className="hero__cta">
